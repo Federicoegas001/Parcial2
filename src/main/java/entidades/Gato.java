9784d70c8984ea4d21284d322de132226654e62f
Fede Egas <egasagustin@gmail.com>
2025-11-21 16:55:19 -0300

    Clase Gato creada

---
+++ src/main/java/entidades/Gato.java
@@ -0,0 +1,9 @@
+package entidades;
+
+import java.util.Date;
+
+public class Gato extends Mascota{
+    public Gato(String nombre, Date fechaNacieminto, float peso, String recomendaciones) {
+        super(nombre, fechaNacieminto, peso, recomendaciones, "gato");
+    }
+}
